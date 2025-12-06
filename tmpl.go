9ff8c8a1fa6b6f0dc55bd8b272db1bd8e9cc02be
Elliott Lewandowski <erlewandowski@alaska.edu>
2025-12-05 16:26:33 -0900

    Updated template file

--- tmpl.go
+++ tmpl.go
@@ -1,10 +1,11 @@
 // tmpl.go
 // Elliott R. Lewandowski
 // 2025-12-05
-// Template for solving Advent of Code challenges
+// Solution to Advent of Code 2025 day x
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 )
@@ -21,10 +22,18 @@ func main() {
 		log.Fatal(err)
 	}
 
-	solve(string(content))
+	sol1, sol2 := solve(string(content))
+
+	fmt.Println("*************************  RESULTS  *************************")
+	fmt.Println("Part 1: ", sol1)
+	fmt.Println("Part 2: ", sol2)
 
 }
 
-func solve(content string) {
+func solve(content string) (int, int) {
 
+	part_1 := 0
+	part_2 := 0
+
+	return part_1, part_2
 }
