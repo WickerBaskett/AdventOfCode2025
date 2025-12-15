b0c9c9712f520e04149afe1333ad53dad6f29b7a
Elliott Lewandowski <erlewandowski@alaska.edu>
2025-12-15 10:28:28 -0900

    Solved day6, part 1 not working currently

--- day6/sol.go
+++ day6/sol.go
@@ -8,6 +8,8 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -34,6 +36,152 @@ func solve(content string) (int, int) {
 
 	part_1 := 0
 	part_2 := 0
+
+	///////////////////
+	//  Parse Input  //
+	///////////////////
+
+	problems := make([][]string, 0)
+	ops := make([]string, 0)
+
+	for _, line := range strings.Split(content, "\n") {
+		count := 0
+		for _, token := range strings.Split(line, " ") {
+			if token == "" {
+				continue
+			}
+
+			if token == "+" || token == "*" {
+				ops = append(ops, token)
+				continue
+			}
+
+			if len(problems) <= count {
+				problems = append(problems, make([]string, 0))
+			}
+
+			problems[count] = append(problems[count], token)
+			count++
+		}
+	}
+
+	/////////////////////
+	//  Sanity Checks  //
+	/////////////////////
+
+	if len(problems) != len(ops) {
+		log.Fatal("Malformed Input: Number of problems does not match number of operations")
+	}
+
+	//////////////
+	//  Part 1  //
+	//////////////
+
+	problems = make([][]string, 0)
+	ops = make([]string, 0)
+
+	for i, prob := range problems {
+
+		res := 0
+		if ops[i] == "*" {
+			res = 1
+		}
+
+		for value := range prob {
+
+			out, err := strconv.Atoi(prob[value])
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			if ops[i] == "+" {
+				res += out
+			} else {
+				res *= out
+			}
+		}
+
+		part_1 += res
+	}
+
+	//////////////
+	//  Part 2  //
+	//////////////
+
+	// Re-Parse input for new form
+	for _, line := range strings.Split(content, "\n") {
+		count := 0
+		for _, token_rune := range line {
+			token := string(token_rune)
+
+			if token == "" {
+				continue
+			}
+
+			if token == "+" || token == "*" {
+				ops = append(ops, token)
+				continue
+			}
+
+			if len(problems) <= count {
+				problems = append(problems, make([]string, 0))
+			}
+
+			if token == " " {
+				count++
+				continue
+			}
+
+			problems[count] = append(problems[count], token)
+			count++
+		}
+	}
+
+	parsed_problems := make([][]int, 0)
+	count := 0
+	for _, num := range problems {
+		if len(num) == 0 {
+			count++
+			continue
+		}
+
+		if len(parsed_problems) <= count {
+			parsed_problems = append(parsed_problems, make([]int, 0))
+		}
+
+		output := ""
+		for _, char := range num {
+			output += char
+		}
+
+		value, err := strconv.Atoi(output)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		parsed_problems[count] = append(parsed_problems[count], value)
+	}
+
+	fmt.Println(parsed_problems)
+
+	for i, prob := range parsed_problems {
+
+		res := 0
+		if ops[i] == "*" {
+			res = 1
+		}
+
+		for value := range prob {
+
+			if ops[i] == "+" {
+				res += prob[value]
+			} else {
+				res *= prob[value]
+			}
+		}
+
+		part_2 += res
+	}
 
 	return part_1, part_2
 }
