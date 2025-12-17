bc9ed1134073f30ce83f19d600aabc7a256e7ae7
Elliott Lewandowski <erlewandowski@alaska.edu>
2025-12-17 11:01:21 -0900

    Created day8

---
+++ day8/sol.go
@@ -0,0 +1,39 @@
+// sol.go
+// Elliott R. Lewandowski
+// 2025-12-15
+// Solution to Advent of Code 2025 day x
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+
+	///////////////////////
+	//  Read Input File  //
+	///////////////////////
+
+	content, err := os.ReadFile("demoinput.txt")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	sol1, sol2 := solve(string(content))
+
+	fmt.Println("*************************  RESULTS  *************************")
+	fmt.Println("Part 1: ", sol1)
+	fmt.Println("Part 2: ", sol2)
+
+}
+
+func solve(content string) (int, int) {
+
+	part_1 := 0
+	part_2 := 0
+
+	return part_1, part_2
+}
