48c846e0af1c507c84a089da3753fecdf8929077
Elliott Lewandowski <erlewandowski@alaska.edu>
2025-12-17 10:55:51 -0900

    Solved part 2 day 7

--- day7/sol.go
+++ day7/sol.go
@@ -67,7 +67,7 @@ func solve(content string) (int, int) {
 	}
 
 	part_1 = sol_part_1(manifold_clone)
-	part_2 = sol_part_2(manifold, start)
+	part_2 = sol_part_2(manifold)
 
 	return part_1, part_2
 }
@@ -98,46 +98,61 @@ func sol_part_1(manifold [][]string) int {
 	return part_1
 }
 
-func sol_part_2(manifold [][]string, pos [2]int) int {
+func sol_part_2(manifold [][]string) int {
 	part_2 := 0
+	cumulative := make([][]int, len(manifold)/2)
+	for i := len(manifold) - 2; i > 1; i -= 2 {
+		for j, char := range manifold[i] {
+			if char == "^" {
+				if i == len(manifold)-2 {
+					cumulative[i/2] = append(cumulative[i/2], 2)
+					continue
+				}
 
-	for x := pos[0]; x < len(manifold)-1; x++ {
+				l := false
+				r := false
+				sum := 0
+				for k := (i / 2) + 1; k < len(cumulative); k++ {
+					if j > 0 && cumulative[k][j-1] > 0 && !l {
+						sum += cumulative[k][j-1]
+						l = true
+					}
 
-		y := pos[1]
+					if j < len(cumulative[k]) && cumulative[k][j+1] > 0 && !r {
+						sum += cumulative[k][j+1]
+						r = true
+					}
 
-		// BASE CASE
-		if x == len(manifold)-2 {
-			part_2 += 1
-		}
+					if l && r {
+						break
+					}
+				}
 
-		// RECURSIVE CASE
+				if !l {
+					sum += 1
+				}
 
-		if manifold[x+1][y] != "^" {
-			manifold[x+1][y] = "|"
-			continue
-		}
+				if !r {
+					sum += 1
+				}
 
-		// Look one tile right
-		if manifold[x+1][y+1] == "." {
-			manifold_clone := make([][]string, 0)
-			for i := range manifold {
-				manifold_clone = append(manifold_clone, slices.Clone(manifold[i]))
+				cumulative[i/2] = append(cumulative[i/2], sum)
+			} else {
+				cumulative[i/2] = append(cumulative[i/2], 0)
 			}
-
-			part_2 += sol_part_2(manifold_clone, [2]int{x + 1, y + 1})
 		}
+	}
 
-		// Look on tile left
-		if manifold[x+1][y-1] == "." {
-			manifold_clone := make([][]string, 0)
-			for i := range manifold {
-				manifold_clone = append(manifold_clone, slices.Clone(manifold[i]))
-			}
+	for i, line := range cumulative {
+		fmt.Println(i, ": ", line)
+	}
 
-			part_2 += sol_part_2(manifold_clone, [2]int{x + 1, y - 1})
+	for val := range cumulative[1] {
+		fmt.Print(cumulative[1][val], " ")
+		if cumulative[1][val] > 0 {
+			part_2 += cumulative[1][val]
+			break
 		}
-
-		break
 	}
 
 	return part_2
